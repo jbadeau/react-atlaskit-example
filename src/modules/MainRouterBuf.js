34ed8a9ef088fc2a8edf000a687c00f935abe199
Artem Sirobaba <sirobabaartem@gmail.com>
2020-03-19 11:36:48 +0200

    first commit

--- src/modules/MainRouterBuf.js
+++ src/modules/MainRouterBuf.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import App from '../components/App';
 import HomePage from '../pages/HomePage';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import SettingsPage from "../pages/SettingsPage";
 import {Grid, GridColumn} from '@atlaskit/page';
 import Subscription from '../pages/Subscription';
 import {UserInfo} from '../pages/UserInfo';
 import {Provider} from 'react-redux';
-import history from '../history/history';
-import { useDispatch, useSelector } from 'react-redux';
 
 
 
@@ -23,23 +21,21 @@ window.store = store;
 
 const MainRouterBuf = () => (
     <Provider store={store}>
-        <React.Fragment>
-        <Router history={history}>
+        <Router>
             <Grid layout="fluid" spacing="compact">
                 <GridColumn medium={4}>
                     <App/>
                 </GridColumn>
                 <GridColumn medium={8}>
-                    <Routes>
-                        <Route path="/" element={<HomePage/>}/>
-                        <Route path="/us" element={<UserInfo/>}/>
-                        <Route path="/sub" element={<Subscription/>}/>
-                        <Route path="/set" element={<SettingsPage/>}/>
-                    </Routes>
+                    <Switch>
+                        <Route path="/" element={HomePage}/>
+                        <Route path="/us" element={UserInfo}/>
+                        <Route path="/sub" element={Subscription}/>
+                        <Route path="/set" element={SettingsPage}/>
+                    </Switch>
                 </GridColumn>
             </Grid>
         </Router>
-        </React.Fragment>
     </Provider>
 );
 
